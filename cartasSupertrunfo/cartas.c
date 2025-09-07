9c320afff9ab27c4e17aa3f647b4536df1b5c402
viniciusjucio-pixel <viniciusjucio@gmail.com>
2025-09-07 20:55:34 +0000

    criei variaveis carta1

---
+++ cartasSupertrunfo/cartas.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    //Declaração de variaveis carta1.
+
+    char estado1;
+    char codigo1[04];
+    char nomeCidade1[50];
+    int populacao1;
+    float area1;
+    float pib1;
+    int NumPontosturisticos1;
+    
+
+
+}
